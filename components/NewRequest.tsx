034ac10255ef0be942562d177dd7361db668a7e9
Nicholas Radtke <raddnc@gmail.com>
2023-04-06 17:06:27 -0500

    created function for newRequests that stores in the db

--- components/NewRequest.tsx
+++ components/NewRequest.tsx
@@ -1,12 +1,29 @@
+'use client';
 import { PlusIcon } from "@heroicons/react/24/solid"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { addDoc, collection, serverTimestamp} from "firebase/firestore";
+import {db} from "../firebase"
 
-function NewChat() {
+function NewRequest() {
+  const router = useRouter();
+  const {data:session} = useSession();
+  
+  const createNewRequest = async() => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "request"),{
+        userId: session?.user?.email!,
+        createAt: serverTimestamp(),
+      }
+    ) 
+    router.push('/request/@{doc.id}')
+  }
   return (
-    <div className="border-grey-500 border chatRow">
+    <div onClick={createNewRequest} className="border-grey-500 border chatRow">
         <PlusIcon className="h-8 w-8"/>
         <p>New Request</p> 
     </div>
   )
 }
 
-export default NewChat
+export default NewRequest
